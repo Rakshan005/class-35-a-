c76127f37b08798c160829eb28ba31d48b8e871b
Rakshan005 <73469597+Rakshan005@users.noreply.github.com>
2020-12-31 17:54:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+var magicalball;
+var database, position;
+
+function setup(){
+    database = firebase.database();
+    createCanvas(500,500);
+    magicalball = createSprite(250,250,10,10);
+    magicalball.shapeColor = "red";
+    var magicalBallPosition = database.ref("ball/position");
+    magicalBallPosition.on("value",readPosition,showError);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+    database.ref("ball/position").set({
+        'x':position.x + x,
+        'y':position.y + y,
+    })
+    
+}
+function readPosition(data){
+    position = data.val();
+    magicalball.x = position.x;
+    magicalball.y = position.y; 
+
+    
+
+}
+function showError(){
+    console.log("error");
+    
+}
